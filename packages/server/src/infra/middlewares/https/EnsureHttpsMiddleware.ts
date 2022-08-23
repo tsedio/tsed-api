da4476e28dc1d38d57fadfaaf7ab12e4cc959070
Romain Lenzotti <rom.lenzotti@gmail.com>
2022-08-23 15:28:04 +0200

    fix(server): bump Ts.ED version to v7.0.0-rc.3

--- packages/server/src/infra/middlewares/https/EnsureHttpsMiddleware.ts
+++ packages/server/src/infra/middlewares/https/EnsureHttpsMiddleware.ts
@@ -1,7 +1,7 @@
-import {HeaderParams, IMiddleware, Middleware, Req, Res} from "@tsed/common";
+import {HeaderParams, MiddlewareMethods, Middleware, Req, Res} from "@tsed/common";
 
 @Middleware()
-export class EnsureHttpsMiddleware implements IMiddleware {
+export class EnsureHttpsMiddleware implements MiddlewareMethods {
   use(@HeaderParams("x-forwarded-proto") protocol: string, @HeaderParams("host") host: string, @Req("url") url: string, @Res() res: Res) {
     if (protocol !== "https") {
       res.redirect(`https://${host}${url}`);
